9b3e2f0cc35a282c647ca5c566b10d3b443e1460
tehZevo <tehzevo@users.noreply.github.com>
2024-07-27 09:38:10 -0400

    Simplify skill use logic

--- game/components/player.py
+++ game/components/player.py
@@ -1,11 +1,10 @@
 import pygame
 
 from ..ecs.component import Component
-from .position import Position
 from .physics import Physics
 from .actor import Actor
-from ..actions import Move, Jump, UseSkill
-from ..skills import leap, attack
+from ..actions import Move, Jump
+from ..data.skill_list import skill_list
 
 from ..keybinds import ActionBind, SkillBind
 
@@ -31,33 +30,31 @@ class Player(Component):
         }
 
     def update(self):
-        keys = pygame.key.get_pressed()
-        
         action_binds, skill_binds = pressed_binds(self.keybinds)
         # print(action_binds, skill_binds)
-        x = ("move_right" in action_binds) - ("move_left" in action_binds)
-        
+
         #TODO: only jump on press (make hold not jump so player has to time it?)
         #TODO: this might require adding a flag to action binds that means "only update on press"
-
-        phys = self.get_component(Physics)
-        pos = self.get_component(Position)
+        #TODO: same with skills -- allow buffering but require an extra press
 
         actor = self.get_component(Actor)
+        phys = self.get_component(Physics)
+        
+        move_dir = ("move_right" in action_binds) - ("move_left" in action_binds)
 
-        if "leap" in skill_binds:
-            #actor.act(UseSkill(leap, cast_direction=x))
-            actor.use_skill(leap, override_direction=x)
+        #prioritize skills over other actions
+        if len(skill_binds) > 0:
+            skill_name = list(skill_binds)[0]
+            skill = skill_list[skill_name] #TODO: check for errors
+            actor.use_skill(skill, override_direction=move_dir)
+            
+            return
         
         if phys.on_ground and "jump" in action_binds:
             actor.act(Jump(0.15))
         
-        if phys.on_ground and x != 0:
-            actor.act(Move(x / 100))
+        if phys.on_ground and move_dir != 0:
+            actor.act(Move(move_dir / 100))
         
-        if not phys.on_ground and x != 0:
-            actor.act(Move(x / 1000))
-
-        if "attack" in skill_binds:
-            actor.act(UseSkill(attack, cast_direction=x), buffer=False)
-            
+        if not phys.on_ground and move_dir != 0:
+            actor.act(Move(move_dir / 1000))
---
+++ game/data/skill_list.py
@@ -0,0 +1,7 @@
+import game.skills as skills
+#TODO: i dont like this but ill do it until i come up with a better way
+
+skill_list = {
+    "leap": skills.leap,
+    "attack": skills.attack,
+}
